101ff4ff96dd13d6df92726b2d3d2fa5583d0cbf
Felipe Azevedo <felipe.azevedo@cws.digital>
2024-09-06 15:03:20 -0300

    starting project

---
+++ first_app/lib/dice_roller.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+
+}
