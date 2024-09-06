42b9551f5e492413bd436ff5eda9ecf2531c2ab6
Felipe Azevedo <felipe.azevedo@cws.digital>
2024-09-06 15:15:07 -0300

    Introducing Stateful Widgets

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,19 +1,15 @@
 import 'dart:ui';
 
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer({super.key, required this.colors});
+  const GradientContainer({super.key, required this.colors});
 
   final List<Color> colors;
-  var activeDiceImage = 'assets/images/dice-2.png';
-
-  void rollDice() {
-    activeDiceImage = 'assets/images/dice-4.png';
-  }
 
   @override
   Widget build(context) {
@@ -22,24 +18,8 @@ class GradientContainer extends StatelessWidget {
         gradient: LinearGradient(
             colors: colors, begin: startAlignment, end: endAlignment),
       ),
-      child: Center(
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Image.asset(
-            activeDiceImage,
-            width: 200,
-          ),
-          TextButton(
-            onPressed: rollDice,
-            style: TextButton.styleFrom(
-              padding: const EdgeInsets.only(top: 20),
-              foregroundColor: Colors.white,
-              textStyle: const TextStyle(
-                fontSize: 28,
-              ),
-            ),
-            child: const Text('Roll dice'),
-          ),
-        ]),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
