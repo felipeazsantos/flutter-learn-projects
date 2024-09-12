3b59ea334a7c29b5762355ff13a127a0955c8942
Felipe Azevedo <felipe.azevedo@cws.digital>
2024-09-12 12:14:51 -0300

    Accepting & Passing Functions as Values

--- basics/lib/quiz.dart
+++ basics/lib/quiz.dart
@@ -10,7 +10,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget activeScreen = const StartScreen();
+  Widget activeScreen = const StartScreen(switchScreen);
 
   void switchScreen() {
     setState(() {
--- basics/lib/start_screen.dart
+++ basics/lib/start_screen.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(mainAxisSize: MainAxisSize.min, children: [
-        Image(
+        const Image(
           image: AssetImage("assets/images/quiz-logo.png"),
           width: 300,
           color: Color.fromARGB(150, 255, 255, 255),
         ),
-        SizedBox(height: 80),
-        Text(
+        const SizedBox(height: 80),
+        const Text(
           "Learn Flutter the fun way!",
           style: TextStyle(color: Colors.white, fontSize: 20),
         ),
-        SizedBox(height: 30),
+        const SizedBox(height: 30),
         OutlinedButton.icon(
-          onPressed: () {},
+          onPressed: startQuiz,
           style: OutlinedButton.styleFrom(
             foregroundColor: Colors.white,
           ),
